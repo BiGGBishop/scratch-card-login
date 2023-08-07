ba47f5eb23000139c42976952b8120907f7cb2bd
David Adeyemi Ogunye <oladimejid@gmail.com>
2023-08-07 14:49:16 +0100

    Initial Commit

---
+++ printout.php
@@ -0,0 +1,84 @@
+<?php session_start() ; ?>
+<!DOCTYPE html>
+<html lang="en" class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="UTF-8" />
+        <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">  -->
+        <title>Stock In</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content="Login and Registration Form with HTML5 and CSS3" />
+        <meta name="keywords" content="html5, css3, form, switch, animation, :target, pseudo-class" />
+        <meta name="author" content="Codrops" />
+      <link rel="icon" type="image/png" sizes="16x16" href="../images/logo.jpg">
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link rel="stylesheet" type="text/css" href="css/animate-custom.css" />
+		
+		
+		
+        <style type="text/css">
+<!--
+.text-center {  text-align: center;
+}
+.style15 {color: #000000}
+.style16 {
+	color: #00CC66;
+	font-weight: bold;
+	font-size: 36px;
+}
+-->
+        </style>
+
+</head>
+
+<body>
+
+        <div class="container">
+            <!-- Codrops top bar -->
+            <div class="codrops-top">
+                <a href=""></a>
+                <span class="right">
+                    <a href="#">
+                        <strong></strong>                    </a>                </span>
+                <div class="clr"></div>
+          </div><!--/ Codrops top bar -->
+          <header>
+                <h1> <span></span></h1>
+			<h1><span></span></h1>
+				
+				
+				<h2>&nbsp;</h2>
+				<h2>&nbsp;</h2>
+				<h2>STOCK IN<span></span></h2>
+				<nav class="codrops-demos">
+			    <div align="left"></div>
+			  </nav>
+          </header>
+            <section>				
+              <div id="container_demo" >
+                    <p>
+                      <!-- hidden anchor to stop jump http://www.css3create.com/Astuce-Empecher-le-scroll-avec-l-utilisation-de-target#wrap4  -->
+                    </p>
+                    <p class="style15"><a href="index.php">HOME</a> | <a href="view-pin.php">View PIN </a></p>
+                    <p class="style15">&nbsp;</p>
+                    <p class="style15">&nbsp;</p>
+                    <p class="style15">&nbsp;</p>
+                    <p class="style15">&nbsp;</p>
+                    <p class="style16">Congratulation, PIN validation is correct.</p>
+                    <p class="style15">&nbsp;</p>
+                    <p class="style15">&nbsp; </p>
+                    <p class="style15">&nbsp;</p>
+                  <p align="center">&nbsp;</p>
+                  <table width="0" border="0" align="center">
+                    <tr>
+                      <td width="134"><a class = "btn btn-success btn-print" href = "" onclick = "window.print()"><i class ="glyphicon glyphicon-print"></i> Print</a></td>
+                      <td width="175"><div align="center"><a href="../logout.php">close</a></div></td>
+                    </tr>
+                  </table>
+              </div>  
+            </section>
+        </div>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+</body>
+</html>
